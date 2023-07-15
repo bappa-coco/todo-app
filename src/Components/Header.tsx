8a6e9320ccc696ddc376fe390b71b9065d10c2d1
bappa-coco <soumitra29maity@gmail.com>
2023-07-16 03:48:31 +0530

    #1 :  create a basic todo app

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,14 +1,28 @@
-import {  AppBar, Toolbar, Typography } from '@mui/material';
-import React from 'react';
 
+import React from 'react';
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
+import AddTaskIcon from '@mui/icons-material/AddTask';
 
 const Header = () => {
     return (
-        <AppBar position="fixed">
-            <Toolbar>
-                <Typography  variant="h5" align="center">Todo App</Typography>
-            </Toolbar>
-        </AppBar>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <AddTaskIcon />
+                    </IconButton>
+                    <Typography variant="h4" component="div" sx={{ flexGrow: 1, pl: 150 }}>
+                        Todo App
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
     )
 }
 
