e3eeba78e506533c5748e20ea1da5cb8053ea575
soumitra-technicise <staywithsoumitra@gmail.com>
2023-08-24 22:07:22 +0530

    change ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,35 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import TodoListItem from "./Components/TodoListItem";
+import { Card,  Grid, Typography } from "@mui/material";
 
 function App() {
   return (
     <>
       <Header />
-      <div className="todo-app">
+      {/* <div className="todo-app">
         <header>
           <h1>Todo List Item</h1>
         </header>
         <TodoListItem />
-      </div>
+      </div> */}
+
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        py={3}
+      >
+        <Card sx={{ minWidth: 450 }}>
+          <Grid item md={12} sm={12} xs={12}>
+            <Typography>Create your todo</Typography>
+          </Grid>
+          <Grid item md={12} sm={12} xs={12}>
+            <TodoListItem />
+          </Grid>
+        </Card>
+      </Grid>
     </>
   );
 }
--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -4,7 +4,6 @@ import {
   Grid,
   InputLabel,
   Input,
-  Typography,
   Container,
 } from "@mui/material";
 import React from "react";
@@ -22,13 +21,6 @@ const TodoForm = ({ handleSubmit, setInputValue, inputValue }: Props) => {
       handleSubmit();
     }
   };
-  const [emailFieldError, setEmailFieldError] = React.useState(false);
-  const emailFieldNullMsg = "Please complete this required field";
-  const emailFieldChange = (e: any) => {
-    e.target.value.length > 0
-      ? setEmailFieldError(false)
-      : setEmailFieldError(true);
-  };
 
   return (
     <Container maxWidth="sm">
@@ -39,23 +31,16 @@ const TodoForm = ({ handleSubmit, setInputValue, inputValue }: Props) => {
             <Input
               id="emailFieldID"
               required
-              error={emailFieldError}
               value={inputValue}
-              onBlur={(e) => emailFieldChange(e)}
               onChange={handleInputChange}
               onKeyDown={handleKeyDown}
               fullWidth
             />
-            {emailFieldError === true ? (
-              <Typography style={{ color: "#d32f2f" }} sx={{ fontSize: 13 }}>
-                {emailFieldNullMsg}
-              </Typography>
-            ) : null}
           </FormControl>
         </Grid>
         <Grid item md={12} sm={12} xs={12}>
           <Button
-            variant="outlined"
+            variant="contained"
             href="#contained-buttons"
             onClick={handleSubmit}
           >
